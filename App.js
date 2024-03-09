94a8d969a241a435fa745bc5cb01b73b84d16e68
AlmeidaRafa21 <161901334+AlmeidaRafa21@users.noreply.github.com>
2024-03-08 22:36:04 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Text style={estilos.texto}>
+        F A Ç A  L O G I N
+      </Text>
+      <Image
+        style={styles.tinyLogo}
+        source={require('@expo/')}
+      />
+
+
+
+      <TextInput style={estilos.input} placeholder="Usuário" />
+      <TextInput style={estilos.input} placeholder="Senha" />
+
+
+
+      <TouchableOpacity style={estilos.button}>
+        <Text>
+          Entrar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5CC6BA',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 8
   },
+  texto: {
+    color: "#BLACK",
+    fontSize: 30, 
+    marginBottom : 25
+  },
+  input: {
+    borderWidth: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 14,
+    borderRadius: 16,
+    backgroundColor: "#fff",
+    marginBottom : 20
+
+  },
+
+  button: {
+    backgroundColor: "#179042",
+    paddingHorizontal: 70,
+    paddingVertical: 10,
+    borderRadius: 16,
+  }
 });
